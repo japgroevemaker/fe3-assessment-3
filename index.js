d6acc12f95fd633aab17ed0caec21bb04a6d1e00
japgroevemaker <joepgravemaker@gmail.com>
2017-10-19 12:14:45 +0200

    Add @japgroevemaker

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-# Assesment 3
-Voor assesment 3 heb ik deze bar chart gekozen van de d3 gallery op github ['https://bl.ocks.org/mbostock/3885304']
-Vervolgens ben ik op het CBS gaan zoeken naar een bruikbare dataset. Dat is deze geworden ['http://statline.cbs.nl/statweb/publication/?vw=t&dm=slnl&pa=37506wwm&d1=0-4&d2=0&d3=0&d4=(l-24)-l&hd=080402-1211&hdr=t&stb=g1,g2,g3']
+/* global d3 */
 
-## Werkwijze
-Omdat ik 2 barcharts wil laten zien, ben ik eerst begonnen met het declareren van beide. In de HTML heb ik beide een id gegeven en daarna ben ik ze binnen de javascript gaan aanroepen.
+//Hier declareer ik de waardes voor de linker grafiek
 
-```var svgLeft = d3.select("#SVGleft"),
+var svgLeft = d3.select("#SVGleft"),
     marginL = {
         top: 20,
         right: 20,
@@ -38,27 +35,26 @@ var xR = d3.scaleBand().rangeRound([0, widthR]).padding(0.1),
     yR = d3.scaleLinear().rangeRound([heightR, 0]);
 
 var gR = svgRight.append("g")
-    .attr("transform", "translate(" + marginR.left + "," + marginR.top + ")");```
-    
-Toen heb ik de code getypt die mijn dataset aanroept
-```var data = d3.text('data.csv')
-    .get(onload);```
+    .attr("transform", "translate(" + marginR.left + "," + marginR.top + ")");
 
-Daarna ben ik bezig gegaan met het opschonen van de data. Dit heb ik gedaan door dit stuk code toe te voegen.
+var data = d3.text('data.csv')
+    .get(onload);
 
-```var header = doc.indexOf('65 jaar');
+function onload(err, doc) {
+    if (err) {
+        throw err
+    }
+
+    var header = doc.indexOf('65 jaar');
     var end = doc.indexOf('Centraal Bureau voor de Statistiek') - 3;
     doc = doc.substring(header, end).trim();
     doc = doc.replace(/;+/g, ',')
     doc = doc.replace(/ +/g, ',')
     doc = doc.replace(/2014,/g, '')
-    var data = d3.csvParseRows(doc, map).slice(0, 12);```
-    
-Ik heb de ```.slice``` gebruikt om de data van het jaar 2014 er tussenuit te halen.
+    var data = d3.csvParseRows(doc, map).slice(0, 12);
 
-Daarna heb ik gedeclareerd wat voor soort data er uit de ```data.csv``` file gehaald moet worden
 
-```function map(d) {
+    function map(d) {
         return {
             Maand: (d[2]),
             Totaal: Number(d[3]),
@@ -67,17 +63,23 @@ Daarna heb ik gedeclareerd wat voor soort data er uit de ```data.csv``` file geh
             West: Number(d[6]),
             Zuid: Number(d[7])
         }
-    }```
-    Hier vertel ik welke data er in mijn rechter grafiek getoond moet worden
-    ```xL.domain(data.map(function (d) {
+    }
+
+
+    //Hier vertel ik welke data grafiek 1 moet laten zien
+
+    xL.domain(data.map(function (d) {
         return (d.Maand );
     }));
     yL.domain([0, d3.max(data, function (d) {
         return d.Totaal;
-    })]);```
+    })]);
+
+//    gL.append("g")
+//        .attr("class", "axis axis--x")
+//        .attr("transform", "translate(0," + heightL + ")")
+//        .call(d3.axisBottom(xL));
     
-Daarna ben ik mijn grafiek verder gaan bouwen
-    ```
     gL.append("g")
         .attr("class", "axis axis--x")
         .attr("transform", "translate(0," + heightL + ")")
@@ -85,18 +87,18 @@ Daarna ben ik mijn grafiek verder gaan bouwen
         .selectAll("text")
         .style("text-anchor", "end")
         .attr("dx", "2em")
-        .attr("dy", "1em");```
+        .attr("dy", "1em");
 
-    ```gL.append("g")
+    gL.append("g")
         .attr("class", "axis axis--y")
         .call(d3.axisLeft(yL).ticks(20))
         .append("text")
         .attr("transform", "rotate(-90)")
         .attr("y", 12)
         .attr("dy", "0.71em")
-        .attr("text-anchor", "end")```
+        .attr("text-anchor", "end")
 
-   ``` gL.selectAll(".bar1")
+    gL.selectAll(".bar1")
         .data(data)
         .enter().append("rect")
         .attr("class", "bar1")
@@ -114,26 +116,28 @@ Daarna ben ik mijn grafiek verder gaan bouwen
         })
         .attr("y", function (d) {
             return yL(d.Totaal);
-        })```
+        })
 
-        ```.attr("height", function (d) {
+        .attr("height", function (d) {
             return heightL - yL(d.Totaal);
-        });```
+        });
 
-Vervolgens heb ik een variabelen ```data2``` aangemaak waar ik de data voor grafiek 2 in ga stoppen. Daarna heb ik er voor gezorgd dat de benodigde data uit mijn data.csv file gepakt gaat worden.
-
-
-```var data2 = [];
+//Hier maak ik een data2 variabel waar ik de data voor de rechtergrafiek in ga stoppen 
+//in variabelen totalCountry 
+    
+    var data2 = [];
     var totalCountry = [];
     var country = '';
     data.forEach(function (d) {
         totalCountry.push(d.Maand);
-    })```
+    })
 
-    ```country = totalCountry[0];
-    data2 = data.filter(filterCountry);```
+    country = totalCountry[0];
+    data2 = data.filter(filterCountry);
+
+    //Hieronder zoek ik binnen mijn data.csv file en pak ik de waardes die ik nodig heb voor mijn rechter grafiek
     
-    ```var noordObject = {};
+    var noordObject = {};
     var zuidObject = {};
     var oostObject = {};
     var westObject = {};
@@ -149,12 +153,109 @@ Vervolgens heb ik een variabelen ```data2``` aangemaak waar ik de data voor graf
     newData.push(noordObject);
     newData.push(zuidObject);
     newData.push(oostObject);
-    newData.push(westObject);```
-    
-Vervolgens heb ik er voor gezorgd dat als men op de linker grafiek bij het totaal klikt, dat er dan in de rechtergrafiek de waardes per landdeel worden laten zien.
+    newData.push(westObject);
 
-```d3.selectAll(".bar1").on('click', onChange);
-function onChange(e) {
+    console.log(newData);
+
+
+//Hieronder vertel ik dat newData in grafiek 2 laten zien moet worden
+
+    //    svgRight
+    xR.domain(newData.map(function (d) {
+        return d.name;
+    }));
+    yR.domain([0, d3.max(data, function (d) {
+        return d.Totaal;
+    })]);
+
+    //  gR.append("g")
+    //      .attr("class", "axis axis--x")
+    //      .attr("transform", "translate(0," + heightR + ")")
+    //      .call(d3.axisBottom(xR));
+
+    //    gR.append("g")
+    //        .attr("class", "axis axis--x")
+    //        .attr("transform", "translate(0," + heightL + ")")
+    //        .call(d3.axisBottom(xL))
+    //        .selectAll("text")
+    //        .style("text-anchor", "end")
+    //        .attr("dx", "2em")
+    //        .attr("dy", "1em");
+
+//    gR.append("g")
+//        .attr("class", "axis axis--y")
+//        .call(d3.axisLeft(yL).ticks(5))
+//        .append("text")
+//        .attr("transform", "rotate(-90)")
+//        .attr("y", 3)
+//        .attr("dy", "0.71em")
+//        .attr("text-anchor", "end")
+//        .attr("transform", "rotate(-90)")
+//        .text("Overledenen");
+//
+//    gR.append("g")
+//        .attr("class", "axis axis--y")
+//        .call(d3.axisLeft(yR).ticks(10))
+//        .append("text")
+//        .attr("transform", "rotate(-90)")
+//        .attr("y", 6)
+//        .attr("dy", "0.71em")
+//        .attr("text-anchor", "end")
+//        .text("Frequency");
+    
+    gR.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(yR).ticks(5))
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 5)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .attr("transform", "rotate(-90)")
+    
+    gR.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + heightR + ")")
+        .call(d3.axisBottom(xR))
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "1em")
+        .attr("dy", "1em");
+
+
+    gR.selectAll(".bar2")
+        .data(newData)
+        .style('fill', 'red')
+        .enter().append("rect")
+        .attr("class", "bar2")
+        .attr("x", function (d) {
+            return xR(d.name);
+        })
+        .attr("y", function (d) {
+            return yR(d.number);
+        })
+        .attr("width", xR.bandwidth())
+        .attr("height", function (d) {
+            return heightR - yR(d.number);
+        });
+
+    //  gR.selectAll(".bar")
+    //    .data(data)
+    //    .enter().append("rect")
+    //      .attr("class", "bar")
+    //      .attr("x", function(d) { return xR(d.Maand); })
+    //      .attr("y", function(d) { return yR(d.Totaal); })
+    //      .attr("width", xR.bandwidth())
+    //      .attr("height", function(d) { return heightR - yR(d.Totaal); });
+
+
+    // hieronder maak ik een click event aan die er voor gaat zorgen dat als ik op een bar klik, dat de data van Noord, Zuid, Oost en West wordt laten zien.
+
+    d3.selectAll(".bar1").on('click', onChange);
+
+    // In de function hieronder wordt er een class active toegevoegd die er voor zorgt dat de data uit de geklikte bar 'active' wordt en getoond wordt.
+
+    function onChange(e) {
         country = e.Maand;
         this.classList.add("active");
         data2 = [];
@@ -172,7 +273,9 @@ function onChange(e) {
         newData.push(zuidObject);
         newData.push(oostObject);
         newData.push(westObject);
-var barElement = gR.selectAll(".bar2")
+
+
+        var barElement = gR.selectAll(".bar2")
             .data(newData);
         barElement.transition().duration(750)
             .attr("class", "bar2")
@@ -187,35 +290,47 @@ var barElement = gR.selectAll(".bar2")
                 return heightR - yR(d.number);
             });
     }
-function filterCountry(d) {
-        return (d.Maand === country);
-    };```
-    
-Daarna heb ik er ook nog voor gezorgd dat de bars van de linker grafiek gesorteerd kunnen worden
 
-```d3.select("input").on("change", change);
+    function filterCountry(d) {
+        return (d.Maand === country);
+    };
+    
+    
+    
+     d3.select("input").on("change", change);
 
   var sorteer = (function() {
     d3.select("input").property("checked", true).each(change);
   });
-function change() {
+
+  function change() {
     clearTimeout(sorteer);
+
+    // Copy-on-write since tweens are evaluated after a delay.
     var x0 = xL.domain(data.sort(this.checked
         ? function(a, b) { return b.Totaal - a.Totaal; }
         : function(a, b) { return d3.ascending(a.Maand, b.Maand); })
         .map(function(d) { return d.Maand; }))
         .copy();
-      svgLeft.selectAll(".bar1")
+      
+
+    svgLeft.selectAll(".bar1")
         .sort(function(a, b) { return x0(a.Maand) - x0(b.Maand); });
-var transition = svgLeft.transition().duration(750),
+
+    var transition = svgLeft.transition().duration(750),
         delay = function(d, i) { return i * 50; };
-transition.selectAll(".bar1")
+
+    transition.selectAll(".bar1")
         .delay(delay)
         .attr("x", function(d) { return x0(d.Maand); });
-      transition.selectAll(".axis--x")
+      
+
+    transition.selectAll(".axis--x")
         .call(d3.axisBottom(xL))
       .selectAll("g")
         .delay(delay);
-  }```
+  }
+};
+
     
     
